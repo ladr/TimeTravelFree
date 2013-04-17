3d81a35762a1e357816b9de69d1f375d4ff21d40
vladl <vladlen.lychak@gmail.com>
2013-04-17 15:31:53 +0300

    Small commit: repaired indenting in MainActivity
    Testing commit from EGit to Github

--- src/com/octops/timetravelfree/MainActivity.java
+++ src/com/octops/timetravelfree/MainActivity.java
@@ -10,31 +10,30 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-@Override
-public void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_main);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-final CustomDigitalClock dc = (CustomDigitalClock) findViewById(R.id.fragment_clock_digital);
-dc.setText(dc.getText().toString());
-final TextView tv=(TextView) findViewById(R.id.textView1);
+		final CustomDigitalClock dc = (CustomDigitalClock) findViewById(R.id.fragment_clock_digital);
+		dc.setText(dc.getText().toString());
+		final TextView tv=(TextView) findViewById(R.id.textView1);
 
 
+		dc.setClickable(true);
+		dc.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				tv.setText(dc.getText().toString());
+				tv.setTextColor(Color.RED);
+			}
+		});
+	}
 
-dc.setClickable(true);
-dc.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View v) {
-// TODO Auto-generated method stub
-tv.setText(dc.getText().toString());
-tv.setTextColor(Color.RED);
-}
-});
-}
-
-@Override
-public boolean onCreateOptionsMenu(Menu menu) {
-getMenuInflater().inflate(R.menu.activity_main, menu);
-return true;
-}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 }
